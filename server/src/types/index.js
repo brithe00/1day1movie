771c521e2a833ad6d12330435c911fe139458347
brithe00 <bthellier51@gmail.com>
2023-11-05 15:59:32 +0100

    added types for my graphql schema

--- server/src/types/index.js
+++ server/src/types/index.js
@@ -2,7 +2,35 @@ import { gql } from 'graphql-tag';
 
 export const typeDefs = gql`
 	#graphql
+	type User {
+		id: ID!
+		username: String!
+		email: String!
+	}
+
+	type AuthPayload {
+		user: User!
+		token: String!
+	}
+
+	input RegisterInput {
+		username: String!
+		email: String!
+		password: String!
+	}
+
+	input LoginInput {
+		username: String!
+		password: String!
+	}
+
 	type Query {
-		hello: String
+		users: [User!]!
+		me: User
+	}
+
+	type Mutation {
+		register(input: RegisterInput!): AuthPayload!
+		login(input: LoginInput!): AuthPayload!
 	}
 `;
