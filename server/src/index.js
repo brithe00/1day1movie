e63ddb6a0414d980f72679be57696c2b9c6d51c3
brithe00 <bthellier51@gmail.com>
2023-11-26 16:48:03 +0100

    added the context function to check if there is a user and prisma, and added the admin resolver

--- server/src/index.js
+++ server/src/index.js
@@ -3,18 +3,34 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './types/index.js';
 import { UserResolvers } from './resolvers/userResolvers.js';
 import { prisma } from './config/db.js';
+import jwt from 'jsonwebtoken';
+import { AdminResolvers } from './resolvers/adminResolvers.js';
 
 const start = async () => {
 	const server = new ApolloServer({
 		typeDefs,
-		resolvers: [UserResolvers],
+		resolvers: [UserResolvers, AdminResolvers],
 	});
 
 	const { url } = await startStandaloneServer(server, {
-		context: async () => ({
-			prisma,
-		}),
-		listen: { port: 4002 },
+		context: async ({ req }) => {
+			let user = null;
+
+			const authorization = req.headers.authorization;
+
+			if (authorization) {
+				const token = authorization.replace('Bearer ', '');
+
+				try {
+					user = jwt.verify(token, process.env.JWT_SECRET);
+				} catch (error) {
+					console.error('Invalid token', error);
+				}
+			}
+
+			return { prisma, user };
+		},
+		listen: { port: 4000 },
 	});
 
 	console.log(`🚀  Server ready at: ${url}`);
